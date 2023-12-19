ace2dd770e8db5d24953a5c334c238d45f2de031
Darshan Jeram <darshan.jeram@gmail.com>
2023-12-19 22:14:13 +0200

    img

--- src/components/about.js
+++ src/components/about.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import logoimg from '../media/logo.jpeg'
 
 function About() {
   return (
     <div className="main" id="about">
       <div className="main__container">
         <div className="main__img--container">
-          <div className="main__img--card"><i className="fas fa-layer-group"></i></div>
+          <img className="about__img" src={logoimg} alt="logo" />
         </div>
         <div className="main__content">
           <h1>About</h1>
           <h2>Read our Story...</h2>
           <p>RUSH WASH was started in 1988 by a young man named Rushil Makan...</p>
-          <button className="main__btn"><a href="#">Contact</a></button>
         </div>
       </div>
     </div>
